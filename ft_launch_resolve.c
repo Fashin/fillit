f68e4fb11741d489d11d4d8e8b69f0d56116cd28
Cyprian BEAUVOIS <cbeauvoi@e1r11p16.42.fr>
2016-12-14 12:14:58 +0100

    not very advanced

--- ft_launch_resolve.c
+++ ft_launch_resolve.c
@@ -6,16 +6,38 @@
 /*   By: cbeauvoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 11:45:34 by cbeauvoi          #+#    #+#             */
-/*   Updated: 2016/12/14 12:01:39 by cbeauvoi         ###   ########.fr       */
+/*   Updated: 2016/12/14 12:14:16 by cbeauvoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+static char		**ft_set_map(int size)
+{
+	char 	**map;
+	int		i;
+
+	i = -1;
+	if ((map = (char **)malloc(sizeof(char *) * size)) == 0)
+		return (0);
+	while (map[++i])
+		ft_bzero((void *)map[i], size);
+	return (map);
+}
+
 int				ft_launch_resolve(char *content)
 {
 	int		count;
+	char	**map;
+	int		i;
 
+	i = -1;
 	count = ((ft_strlen(content) / 21) + 1);
+	map = ft_set_map(count);
+	while (map[++i])
+	{
+		ft_putstr(map[i]);
+		ft_putchar('\n');
+	}
 	return (0);
 }
